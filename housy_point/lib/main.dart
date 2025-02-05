ef51469c0b46cf8e6b5453ca6c8bd3710ffcada6
Akash Gupta <akashgupta@MacBook-Pro-2.mshome.net>
2025-02-05 10:58:22 +0530

    changes

--- housy_point/lib/main.dart
+++ housy_point/lib/main.dart
@@ -12,7 +12,6 @@ import 'controllers/providers/homeProvider/home_screen_provider.dart';
 import 'controllers/providers/menuProvider/menu_provider.dart';
 import 'package:provider/provider.dart';
 import 'views/screens/bottomNavBarScreens/home_screen.dart';
-import 'views/screens/homeLoanScreen/home_loan_screen.dart';
 
 void main() {
   runApp(
